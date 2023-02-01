e2a4ce507561629959692237341ca06bbe358ead
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2023-01-31 21:13:32 -0600

    chore: basic typechecking on resources

--- index.ts
+++ index.ts
@@ -1,4 +1,30 @@
 import resources from './data/resources.json';
+
+type Link = {
+  type: string;
+  href: string;
+}
+
+type Resource = {
+  name: string;
+  description: string;
+  category: string;
+  website?: {
+    href: string;
+    title: string;
+  }
+  links?: Link[]
+  image?: {
+    src: string
+  }
+}
+
+/**
+ * This is a bit of a hack to run some typechecking against
+ * the `resource.json` file without too much work!
+ */
+const _resources: Resource[] = resources;
+
 export default {
   resources
 }
